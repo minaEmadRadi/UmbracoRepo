2befe08e261cc7c5c761b0d894e0c92c2299fde5
Mina Radi <Mina.Radi@linkdev.com>
2025-01-21 19:38:27 +0200

    js seperation

---
+++ Umbraco/wwwroot/js/blogApiService.js
@@ -0,0 +1,28 @@
+﻿class BlogApiService {
+    static async fetchBlogItems(pageNumber ) {
+        const skip = (pageNumber - 1) * BlogGlobals.pagination.itemsPerPage;
+
+        try {
+            const response = await $.ajax({
+                url: `${window.location.origin}${BlogGlobals.api.endpoints.content}`,
+                method: "GET",
+                timeout: 0,
+                headers: {
+                    "Accept-Language": window.currentLanguage,
+                    "Api-Key": BlogGlobals.api.headers.apiKey
+                },
+                data: {
+                    filter: `contentType:${BlogGlobals.umbraco.itemAlias}`,
+                    skip: skip,
+                    take: BlogGlobals.pagination.itemsPerPage
+                }
+            });
+
+            return response;
+        } catch (error) {
+            console.error('Error fetching blog items:', error);
+            throw new Error(BlogGlobals.localization.translations[window.currentLanguage].errors.loadFailed);
+        }
+    }
+
+}
